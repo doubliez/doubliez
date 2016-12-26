c10dfb8b35f8249ea55e6296301714ca99100b3f
Thibault Doubliez <thibault@doubliez.com>
2016-12-26 12:08:17 +0100

    Dynamic title

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule Doubliez.LayoutView do
   use Doubliez.Web, :view
+
+  @doc """
+  Calls the `title` fun of the current `view_module` with the performed `:action` as arg.
+  """
+  def title(conn) do
+    apply view_module(conn), :title, [action_name(conn)]
+  end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,6 +1,8 @@
 defmodule Doubliez.PageView do
   use Doubliez.Web, :view
 
+  def title(_), do: full_name
+
   def full_name do
     "Thibault Doubliez"
   end
